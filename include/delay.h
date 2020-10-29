f0006f99d18be357c34e0029a519b3d7f67427ad
alexthesys <alexsuprunov@gmail.com>
2020-10-29 04:36:13 +0300

    Streamline processing

--- include/delay.h
+++ include/delay.h
@@ -1,19 +1,23 @@
 #ifndef DELAY_H
 #define DELAY_H
 
-#include <memory>
+#include <cstring>
 #include <vector>
 #include "constants.h"
-#include <cstring>
+
 
 class DelaySIMD
 {
-    std:: unique_ptr<std::vector<float>> delayBuffer[2];
-    typedef struct {__m128 mWet, mDry, mFb;} DCoeffs;
-    alignas (16) DCoeffs dCoeffs;
+    typedef struct {
+        alignas(16) float mWet[4];
+        alignas(16) float mDry[4];
+        alignas(16) float mFb[4];
+    } DCoeffs;
+    alignas(16) DCoeffs dCoeffs;
+    std::vector<float> delayBuffer;
     size_t mReadIndex[2], mWriteIndex[2];
     size_t delay_buff_size, delay_buff_mask;
-    __m128 extFB;
+
     static size_t ms2samples(const int) noexcept;
     template<typename Width>
     static Width findNextPow2(Width v) noexcept;
@@ -21,12 +25,9 @@ class DelaySIMD
 public:
     DelaySIMD(const double);
     void updateDelay(float*, const int) noexcept;
-    void updateDelayExtFB(float*, const int) noexcept;
     void setOffset(const double) noexcept;
     void setDryWet(const float) noexcept;
     void setFeedback(const float) noexcept;
-    __m128& getDelayedSample(const int ch) const noexcept;
-    void setExternalFB(float fb) noexcept;
     void flushDelayBuffers() noexcept;
 };
 
@@ -39,19 +40,23 @@ inline void DelaySIMD::setOffset(const double time) noexcept
 
 inline void DelaySIMD::setDryWet(const float dw) noexcept
 {
-    dCoeffs.mWet = _mm_set1_ps(dw);
-    dCoeffs.mDry = _mm_set1_ps(1 - dw);
+    const float neg = 1 - dw;
+    for (int i = 0; i < 4; i++) {
+        dCoeffs.mWet[i] = dw;
+        dCoeffs.mDry[i] = neg;
+    }
 }
 
 inline void DelaySIMD::setFeedback(const float fb) noexcept
 {
-    dCoeffs.mFb = _mm_set1_ps(fb);
+    for (int i = 0; i < 4; i++)
+        dCoeffs.mFb[i] = fb;
 }
 
 inline size_t DelaySIMD::ms2samples(const int ms) noexcept
 {
-    float sr = static_cast<float>(audio_tools::SAMPLE_RATE) / 1000.0f;
-    return static_cast<size_t>(static_cast<float>(ms) * sr);
+    const double sr = (audio_tools::SAMPLE_RATE) / 1000.0;
+    return static_cast<size_t>(static_cast<double>(ms) * sr);
 }
 
 template<typename Width>
@@ -67,128 +72,13 @@ Width DelaySIMD::findNextPow2(Width v) noexcept
 
 inline size_t DelaySIMD::multipleOfN(size_t sRate, size_t N)
 {
-    size_t adjust = N - (sRate & (N - 1));
+    const size_t adjust = N - (sRate & (N - 1));
        return (sRate + adjust);
-}
-
-inline __m128& DelaySIMD::getDelayedSample(const int ch) const noexcept
-{
-    return *(reinterpret_cast<__m128*>(&(delayBuffer[ch]->at(
-                                             static_cast<size_t>(mReadIndex[ch])))));
-}
-
-inline void DelaySIMD::setExternalFB(float fb) noexcept
-{
-    extFB = _mm_set1_ps(fb);
 }
 
 inline void DelaySIMD::flushDelayBuffers() noexcept
 {
-    memset(&delayBuffer[0], 0, sizeof (float) * delay_buff_size);
-    memset(&delayBuffer[1], 0, sizeof (float) * delay_buff_size);
-}
-
-//------------------------------------------------------------------------
-
-class DelayFractional
-{
-    std:: unique_ptr<std::vector<float>> delayBuffer[2];
-    typedef struct {float mWet, mDry, mFb;} DCoeffs;
-    DCoeffs dCoeffs;
-    size_t mReadIndex[2], mWriteIndex[2];
-    size_t delay_buff_size, delay_buff_mask;
-    float delayFraction[2];
-    float extFB;
-    static size_t ms2samples(double, float&) noexcept;
-    template<typename Width>
-    static Width findNextPow2(Width v) noexcept;
-    float linearInterp(float, float, float&);
-    void updateIndices(int) noexcept;
-    void calculateYn(float, float&, int) noexcept;
-public:
-    DelayFractional(double);
-    void updateDelay(float*, int) noexcept;
-    void updateDelayCrossFB(float*, int) noexcept;
-    void updateDelayExtFB(float*, int) noexcept;
-    void setOffset(double, int) noexcept;
-    void setDryWet(float) noexcept;
-    void setFeedback(float) noexcept;
-    float& getDelayedSample(int ch) const noexcept;
-    void setExternalFB(float fb) noexcept;
-    void flushDelayBuffers() noexcept;
-};
-
-inline void DelayFractional::setOffset(double time, int ch) noexcept
-{
-    mReadIndex[ch] = ((mWriteIndex[ch] - ms2samples(time, delayFraction[ch])) & delay_buff_mask);
-}
-
-inline void DelayFractional::setDryWet(float dw) noexcept
-{
-    dCoeffs.mWet = dw;
-    dCoeffs.mDry = 1.0f - dw;
-}
-
-inline void DelayFractional::setFeedback(float fb) noexcept
-{
-    dCoeffs.mFb = fb;
-}
-
-inline size_t DelayFractional::ms2samples(double ms, float& dFraction) noexcept
-{
-    const float delaySamples = static_cast<float>(audio_tools::SAMPLE_RATE * ms) / 1000.0f;
-    const size_t delayIntegral = static_cast<size_t>(delaySamples);
-    dFraction = delaySamples - static_cast<float>(delayIntegral);
-    return delayIntegral;
-}
-
-template<typename Width>
-Width DelayFractional::findNextPow2(Width v) noexcept
-{
-    --v;
-    for (uint32_t i = 1, maxShift = sizeof (Width) << 3; i <= maxShift; i = i << 1){
-        v |= v >> i;
-    }
-    ++v;
-    return v;
-}
-
-inline float& DelayFractional::getDelayedSample(int ch) const noexcept
-{
-    return delayBuffer[ch]->at(mReadIndex[ch]);
-}
-
-inline void DelayFractional::setExternalFB(float fb) noexcept
-{
-    extFB = fb;
-}
-
-inline void DelayFractional::flushDelayBuffers() noexcept
-{
-    memset(&delayBuffer[0], 0, sizeof (float) * delay_buff_size);
-    memset(&delayBuffer[1], 0, sizeof (float) * delay_buff_size);
-}
-
-inline float DelayFractional::linearInterp(float y0, float y1, float& dFraction)
-{
-    return (y0 *(1.0f - dFraction) + (y1 * dFraction));
-}
-
-inline void DelayFractional::updateIndices(int ch) noexcept
-{
-//    mReadIndex[ch] = (mReadIndex[ch] + 1) & delay_buff_mask;
-    mWriteIndex[ch] = (mWriteIndex[ch] + 1) & delay_buff_mask;
-}
-
-inline void DelayFractional::calculateYn(float xn, float& yn, int ch) noexcept
-{
-    if (mWriteIndex[ch] != mReadIndex[ch]) {
-        const size_t readIndex1 = (mReadIndex[ch] - 1) & delay_buff_mask;
-        yn = linearInterp(delayBuffer[ch]->at(mReadIndex[ch]), delayBuffer[ch]->at(readIndex1), delayFraction[ch]);
-    }
-    else {
-        yn = xn;
-    }
+    memset(delayBuffer.data(), 0, sizeof (float) * delay_buff_size * 2);
 }
 #endif // DELAY_H
 
--- source/delay.cpp
+++ source/delay.cpp
@@ -1,12 +1,10 @@
 #include "../include/delay.h"
 
-
-DelaySIMD::DelaySIMD(const double sr) : extFB(_mm_setzero_ps())
+DelaySIMD::DelaySIMD(const double sr)
 {
     delay_buff_size = findNextPow2(static_cast<size_t>(sr*2.0));	// sr*2 = 2 sec
     delay_buff_mask = delay_buff_size - 4;	// -1
-    delayBuffer[0] = std::make_unique<std::vector<float>>(delay_buff_size);
-    delayBuffer[1] = std::make_unique<std::vector<float>>(delay_buff_size);
+    delayBuffer.resize(delay_buff_size * 2);
     memset(&dCoeffs, 0, sizeof(DCoeffs));
     memset(mWriteIndex, 0, sizeof (size_t)*2);
 }
@@ -17,82 +15,20 @@ void DelaySIMD::updateDelay(float* buffer, const int ch) noexcept
 
     __m128 input = _mm_load_ps(buffer);
 
-    __m128 delayed = _mm_load_ps(&(delayBuffer[ch]->at(mReadIndex[ch])));
-    // code duplication vs branchless
-    __m128 yn = _mm_mul_ps(delayed, dCoeffs.mFb);
+    __m128 delayed = _mm_load_ps(&(delayBuffer[mReadIndex[ch]*(ch+1)]));
+
+    __m128 xmm = _mm_load_ps(dCoeffs.mFb);
+    __m128 yn = _mm_mul_ps(delayed, xmm);
     yn = _mm_add_ps(yn, input);
-    _mm_store_ps(&(delayBuffer[ch]->at(mWriteIndex[ch])), yn);
-    // -------------------------------
-    delayed = _mm_mul_ps(delayed, dCoeffs.mWet);
-    input = _mm_mul_ps(input, dCoeffs.mDry);
+    _mm_store_ps(&(delayBuffer[mWriteIndex[ch]*(ch+1)]), yn);
+
+    xmm = _mm_load_ps(dCoeffs.mWet);
+    delayed = _mm_mul_ps(delayed, xmm);
+    xmm = _mm_load_ps(dCoeffs.mDry);
+    input = _mm_mul_ps(input, xmm);
     input = _mm_add_ps(delayed, input);
     _mm_store_ps(buffer, input);
 
     mReadIndex[ch] = (mReadIndex[ch] + 4) & delay_buff_mask;
     mWriteIndex[ch] = (mWriteIndex[ch] + 4) & delay_buff_mask;
-}
-
-void DelaySIMD::updateDelayExtFB(float* buffer, const int ch) noexcept
-{
-
-    __m128 input = _mm_load_ps(buffer);
-
-    __m128 delayed = _mm_load_ps(&(delayBuffer[ch]->at(mReadIndex[ch])));
-    // code duplication vs branchless
-    extFB = _mm_add_ps(extFB, input);
-    _mm_store_ps(&(delayBuffer[ch]->at(mWriteIndex[ch])), extFB);
-    // ----------------------------------
-    delayed = _mm_mul_ps(delayed, dCoeffs.mWet);
-    input = _mm_mul_ps(input, dCoeffs.mDry);
-    input = _mm_add_ps(delayed, input);
-    _mm_store_ps(buffer, input);
-
-    mReadIndex[ch] = (mReadIndex[ch] + 4) & delay_buff_mask;
-    mWriteIndex[ch] = (mWriteIndex[ch] + 4) & delay_buff_mask;
-}
-
-// ------------------------------------------------------------------------------
-
-DelayFractional::DelayFractional(double sr) : extFB(0.0f)
-{
-    delay_buff_size = findNextPow2(static_cast<size_t>(sr*2.0));	// sr*2 = 2 sec
-    delay_buff_mask = delay_buff_size - 1;
-    delayBuffer[0] = std::make_unique<std::vector<float>>(delay_buff_size);
-    delayBuffer[1] = std::make_unique<std::vector<float>>(delay_buff_size);
-    memset(&dCoeffs, 0, sizeof(DCoeffs));
-    memset(mWriteIndex, 0, sizeof (size_t)*2);
-    memset(delayFraction, 0, sizeof (float)*2);
-}
-
-void DelayFractional::updateDelay(float* buffer, int ch) noexcept
-{
-    const float xn = *buffer;
-    float yn = 0.0f;
-    calculateYn(xn, yn, ch);
-    delayBuffer[ch]->at(mWriteIndex[ch]) = xn + yn * dCoeffs.mFb;
-    *buffer = dCoeffs.mDry * xn + dCoeffs.mWet * yn;
-
-    updateIndices(ch);
-}
-
-void DelayFractional::updateDelayCrossFB(float* buffer, int ch) noexcept
-{
-    const float xn = *buffer;
-    float yn = 0.0f;
-    calculateYn(xn, yn, ch);
-    delayBuffer[ch ^ 0x1]->at(mWriteIndex[ch ^ 0x1]) = xn + yn * dCoeffs.mFb;
-    *buffer = dCoeffs.mDry * xn + dCoeffs.mWet * yn;
-
-    updateIndices(ch);
-}
-
-void DelayFractional::updateDelayExtFB(float* buffer, int ch) noexcept
-{
-    const float xn = *buffer;
-    float yn = 0.0f;
-    calculateYn(xn, yn, ch);
-    delayBuffer[ch]->at(mWriteIndex[ch]) = xn + extFB;
-    *buffer = dCoeffs.mDry * xn + dCoeffs.mWet * yn;
-
-    updateIndices(ch);
 }
